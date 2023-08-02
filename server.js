872a1ccc15004395c8470ec32238b3f5c6c300d0
Prateek <prateek.kumar.original@gmail.com>
2023-08-02 13:08:05 +0530

    Complete API

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Task = require('./taskModel');
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = 3001;
 const db_uri = 'mongodb://127.0.0.1/taskmanagerDB'
 
 mongoose.connect(db_uri)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log('Failed to connect to MongoDB\n', err));
-
 
 app.route('/tasks')
     .get((req, res) => {
@@ -19,7 +19,6 @@ app.route('/tasks')
             .catch(err => res.status(400).json('Error: ' + err));
     })
     .post((req, res) => {
-        console.log(req.body);
         Task.create(req.body)
             .then(task => res.json(task))
             .catch(err => res.status(400).json('Error: ' + err));
@@ -36,25 +35,11 @@ app.route('/tasks/:taskId')
             .then((task) => res.json(task))
             .catch((err) => res.status(400).json('Error: ' + err));
     })
-    .patch((req, res) => {
-        Task.findById(req.params.taskId)
-            .then((task) => {
-                Object.assign(task, req.body);
-                return task.save();
-            })
-            .then((task) => res.json(task))
-            .catch((err) => res.status(400).json('Error: ' + err));
-    })
     .delete((req, res) => {
         Task.findByIdAndDelete(req.params.taskId)
             .then(() => res.json('Task deleted.'))
             .catch(err => res.status(400).json('Error: ' + err));
     });
-
-
-
-
-
 
 
 
