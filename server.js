c5e58a31e6d34fd738a6faa9bb5492e79aacf511
Prateek <prateek.kumar.original@gmail.com>
2023-08-03 00:37:44 +0530

    Create React Frontend and send post request

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Task = require('./taskModel');
+const cors = require('cors');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 const port = 3001;
 const db_uri = 'mongodb://127.0.0.1/taskmanagerDB'
@@ -15,29 +18,29 @@ mongoose.connect(db_uri)
 app.route('/tasks')
     .get((req, res) => {
         Task.find()
-            .then(tasks => res.json(tasks))
+            .then(tasks => res.status(200).json(tasks))
             .catch(err => res.status(400).json('Error: ' + err));
     })
     .post((req, res) => {
         Task.create(req.body)
-            .then(task => res.json(task))
+            .then(task => res.status(201).json(task))
             .catch(err => res.status(400).json('Error: ' + err));
     });
 
 app.route('/tasks/:taskId')
     .get((req, res) => {
         Task.findById(req.params.taskId)
-            .then(task => res.json(task))
+            .then(task => res.status(200).json(task))
             .catch(err => res.status(400).json('Error: ' + err));
     })
     .put((req, res) => {
         Task.findByIdAndUpdate(req.params.taskId, req.body, { new: true })
-            .then((task) => res.json(task))
+            .then((task) => res.status(202).json(task))
             .catch((err) => res.status(400).json('Error: ' + err));
     })
     .delete((req, res) => {
         Task.findByIdAndDelete(req.params.taskId)
-            .then(() => res.json('Task deleted.'))
+            .then(() => res.status(202).json('Task deleted.'))
             .catch(err => res.status(400).json('Error: ' + err));
     });
 
---
+++ task-manager-frontend/src/components/TaskForm.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+const TaskForm = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [status, setStatus] = useState('');
+    const [message, setMessage] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!title || !description || !status) {
+            alert('All fields are required');
+            return;
+        }
+        // Logic to submit the form
+        fetch('http://127.0.0.1:3001/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ title, description, status }),
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        }).then(data => {
+            // handle response data
+            setMessage('Form submitted successfully!');
+        }).catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label className='title'>
+                    Title:
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
+                </label>
+                <label>
+                    Description:
+                    <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" rows="4" cols="100" />
+                </label>
+                <label>
+                    Status:
+                    <select value={status} onChange={e => setStatus(e.target.value)}>
+                        <option value="New">New</option>
+                        <option value="Not Started">Not Started</option>
+                        <option value="In Progress">In Progress</option>
+                        <option value="Completed">Completed</option>
+                    </select>
+                </label>
+                <button type="submit">Add Task</button>
+            </form>
+            {message && <div className="message">{message}</div>}
+        </div>
+    );
+};
+
+export default TaskForm;
